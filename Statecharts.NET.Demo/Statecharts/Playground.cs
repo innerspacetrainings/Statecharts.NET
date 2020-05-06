fb7dd8aec7810af47107ef8ca1b72e87225dce29
bemayr <bernhard@mayr.io>
2020-05-06 21:00:03 +0200

    finally added Playground Example

---
+++ Statecharts.NET.Demo/Statecharts/Playground.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Statecharts.NET.Demo;
+using Statecharts.NET.Language;
+using Statecharts.NET.Model;
+using static Statecharts.NET.Language.Keywords;
+
+
+namespace Statecharts.NET.Demos.Statecharts
+{
+    class Playground
+    {
+        internal static StatechartDefinition<FetchContext> Behaviour => Define.Statechart
+            .WithInitialContext(new FetchContext { Retries = 0 })
+            .WithRootState(
+                "Playground"
+                    .AsCompound()
+                    .WithInitialState("Init")
+                    .WithStates(
+                        "Init"
+                            .AsCompound()
+                            .WithInitialState("First")
+                            .WithStates(
+                                "First".WithTransitions(On("click").TransitionTo.Absolute("Playground", "Init", "Second")),
+                                "Second".WithInvocations(Define.Service.Task(token => Task.Delay(1000, token)).OnSuccess.TransitionTo.Sibling("Third")),
+                                "Third".AsFinal())
+                            .OnDone.TransitionTo.Sibling("Next"),
+                        "Next"));
+    }
+}
